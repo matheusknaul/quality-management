79c365af4c8fd31fed327737f1971cfd8223d362
matheusknaul <contatomatheush0909@gmail.com>
2024-08-20 08:42:07 -0300

    add necessary code

--- quality_managements/views.py
+++ quality_managements/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render('home.html')
+    return render(request, 'quality_managements/home.html')
