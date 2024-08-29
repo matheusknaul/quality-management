bd12a5df1e0a1000ab680262a668dbfa66dfb387
matheusknaul <contatomatheush0909@gmail.com>
2024-08-29 16:40:32 -0300

    refresh

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask,redirect,url_for,render_template,request
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy # type: ignore
+
+# Criação do objeto SQLAlchemy
+db = SQLAlchemy()
+
 app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
 
 @app.route('/',methods=['GET','POST'])
 def home():
