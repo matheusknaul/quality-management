21f8d75da102997dd75f6904f9646e647641579d
matheusknaul <contatomatheush0909@gmail.com>
2024-08-29 10:38:37 -0300

    add math methods

---
+++ analys_critical/math/interpolate.py
@@ -0,0 +1,40 @@
+def sum(conj):
+    sum = 0
+    for value in range(len(conj)):
+        sum = sum + value
+    return sum
+
+def sum_products(conj_x, conj_y):
+    sum = 0
+    for value in range(len(conj_x)):
+        sum = sum + (conj_x[value] * conj_y[value])
+    return sum
+
+def sum_square(conj):
+    sum = 0
+    for value in range(len(conj)):
+        sum = sum + value**2
+    return sum
+
+def inclination(conj_x, conj_y):
+    n_elements = len(conj_x)
+
+    return ((n_elements * sum_products(conj_x, conj_y)) - (sum(conj_x)* sum(conj_y))) / ((n_elements * sum_square(conj_x))- ((sum(conj_x))**2))
+
+def interception(conj_x, conj_y):
+    n_elements = len(conj_x)
+
+    x = sum(conj_x) / n_elements
+    y = sum(conj_y) / n_elements
+
+    return y - (inclination(conj_x, conj_y) * x)
+
+def interpolate(conj_x, conj_y, value):
+    return interception(conj_x, conj_y) + (inclination(conj_x, conj_y) * value)
+
+conj_x = [30.1, 40.5, 51, 61.1, 71.2]
+conj_y = [30, 40, 50, 60, 70]
+
+value = 39
+
+print(interpolate(conj_x, conj_y, value))
--- analys_critical/math/percentage.py
+++ analys_critical/math/percentage.py
@@ -1,2 +1,3 @@
 
-
+def percentual_value(target, percentage):
+    return (percentage / 100) * target
