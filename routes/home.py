70b26ed23771951b7110d2dcef6e473d8a9a3e03
matheusknaul <contatomatheush0909@gmail.com>
2024-09-09 17:04:42 -0300

    create templates

--- routes/home.py
+++ routes/home.py
@@ -5,3 +5,11 @@ home_route = Blueprint('home', __name__)
 @home_route.route('/')
 def home():
     return render_template('home/index.html')
+
+@home_route.route('/nota_fiscal')
+def nota_fisca():
+    return render_template('home/nota_fiscal')
+
+@home_route.route('/solicitar_aprovacao')
+def solicitar_aprovacao():
+    return render_template('home/solicitar_aprovacao')
