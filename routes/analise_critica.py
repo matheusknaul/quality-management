c491ce301eecf271d224ca8ccac55460a1428c40
matheusknaul <contatomatheush0909@gmail.com>
2024-09-10 10:24:32 -0300

    add analys critical templates

--- routes/analise_critica.py
+++ routes/analise_critica.py
@@ -5,3 +5,11 @@ analise_critica_route = Blueprint('analise_critica', __name__)
 @analise_critica_route.route('/')
 def index():
     return render_template('analise_critica/index.html')
+
+@analise_critica_route.route('/manual')
+def analise_manual():
+    return render_template('analise_critica/analise_manual.html')
+
+@analise_critica_route.route('/automatica')
+def analise_automatica():
+    return render_template('analise_critica/analise_automatica.html')
